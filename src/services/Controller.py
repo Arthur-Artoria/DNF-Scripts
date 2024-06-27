3199b0e2d954bc132dca8e8f904b4c7cdd7abd3a
wb.zkx <wb.zkx@mesg.corp.netease.com>
2024-06-27 18:35:10 +0800

    feat: 设计多进程模式

--- src/services/Controller.py
+++ src/services/Controller.py
@@ -1,103 +1,59 @@
 import time
-from typing import Literal, Optional
-import numpy as np
-from numpy._typing import NDArray
-import serial
-import ch9329Comm
-from services import Screen
-from constants import Keyboard, Monitor
+from multiprocessing import Process, Queue
 
-type Offset = dict[Literal["x", "y"], int]
+from services import Screen, Serial
+from constants import Keyboard
 
-__mouse: ch9329Comm.mouse.DataComm
-__keyboard: ch9329Comm.keyboard.DataComm
+
+queue: Queue = Queue()
 
 
 def setup(sleep: bool = True):
     if sleep:
         time.sleep(3)
-    serial.ser = serial.Serial("COM3", 115200)  # type: ignore # 开启串口
-    __initMouse()
-    __initKeyboard()
-    __extendKeyboard()
-
-
-def __initKeyboard():
-    global __keyboard
-    __keyboard = ch9329Comm.keyboard.DataComm()
-
-
-def __extendKeyboard():
-    global __keyboard
-    for value in Keyboard.options.values():
-        if "code" in value:
-            key, code = value.values()
-            __keyboard.normal_button_hex_dict[key] = code
-
-
-def __initMouse():
-    global __mouse
-    __mouse = ch9329Comm.mouse.DataComm(Monitor.SCREEN_WIDTH, Monitor.SCREEN_HEIGHT)
+    Serial.setup()
 
 
 def close():
-    serial.ser.close()  # type: ignore
+    queue.put(None)
 
 
-def press(key: str, seconds: float = 0.5, sleep: float = 0.5):
+def press(key: str):
     data = "".join(map(lambda x: Keyboard.options[x]["key"], key.split(" ")))
-    __keyboard.send_data(data)
-    time.sleep(seconds)
-    __keyboard.release()
-    time.sleep(sleep)
+    queue.put((Serial.INSTRUCTION_PRESS, data, None))
 
 
 def release():
-    __keyboard.release()
+    queue.put((Serial.INSTRUCTION_RELEASE, None, None))
 
 
-def click(
-    locations: tuple[NDArray[np.intp], ...] | Screen.Point | None,
-    offset: Offset | None = None,
-):
-    if locations is None:
-        return
-
-    x, y = locations
-
-    if isinstance(x, np.ndarray) and isinstance(x, np.ndarray):
-        point = Screen.getFirstPoint(locations)  # type: ignore
-    else:
-        point: Screen.Point = locations  # type: ignore
-
-    offsetX, offsetY = 0, 0
-    if offset is not None:
-        if "x" in offset:
-            offsetX = offset["x"]
-        if "y" in offset:
-            offsetY = offset["y"]
-
-    if point is None:
-        return
-
-    x, y = point
-    __mouse.send_data_absolute(x + offsetX, y + offsetY)
-    __mouse.click()
-    time.sleep(0.5)
+def click(point: Screen.Point | None, offset: Serial.Offset | None = None):
+    queue.put((Serial.INSTRUCTION_CLICK, point, offset))
 
 
-def mouseMove(x: int, y: int):
-    __mouse.send_data_absolute(x, y)
-    time.sleep(1)
+def mouseMove(point: Screen.Point):
+    queue.put((Serial.INSTRUCTION_MOUSE_MOVE, point, None))
 
 
-def clickImg(imgPath: str, offset: Offset | None = None):
-    locations = Screen.match(imgPath)
-    click(locations, offset)
+def clickImg(imgPath: str, offset: Serial.Offset | None = None):
+    point = Screen.getFirstPoint(Screen.match(imgPath))
+    click(point, offset)
 
 
 if __name__ == "__main__":
-    setup(False)
+    # setup(False)
+    # press("Up")
+    # close()
+
+    serialProcess = Process(target=Serial.worker, args=(queue,))
+    serialProcess.start()
+
     press("Up")
+    print("11111")
+    release()
+    click((100, 100))
+    mouseMove((100, 100))
+
     close()
+
 #
---
+++ src/services/Serial.py
@@ -0,0 +1,105 @@
+from multiprocessing import Queue
+from typing import Any, Literal, Optional
+import serial
+import ch9329Comm
+from services import Screen
+from constants import Keyboard, Monitor
+
+type Offset = tuple[int, int]
+
+type Instruction = tuple[
+    Literal[1, 2, 3, 4],
+    Any,
+    Optional[Offset],
+]
+
+INSTRUCTION_PRESS = 1
+INSTRUCTION_RELEASE = 2
+INSTRUCTION_CLICK = 3
+INSTRUCTION_MOUSE_MOVE = 4
+
+__mouse: ch9329Comm.mouse.DataComm
+__keyboard: ch9329Comm.keyboard.DataComm
+
+
+def setup():
+    serial.ser = serial.Serial("COM3", 115200)  # type: ignore # 开启串口
+    __initMouse()
+    __initKeyboard()
+    __extendKeyboard()
+
+
+def __initKeyboard():
+    global __keyboard
+    __keyboard = ch9329Comm.keyboard.DataComm()
+
+
+def __extendKeyboard():
+    global __keyboard
+    for value in Keyboard.options.values():
+        if "code" in value:
+            key, code = value.values()
+            __keyboard.normal_button_hex_dict[key] = code
+
+
+def __initMouse():
+    global __mouse
+    __mouse = ch9329Comm.mouse.DataComm(Monitor.SCREEN_WIDTH, Monitor.SCREEN_HEIGHT)
+
+
+def worker(queue: Queue):
+    while True:
+        instruction = queue.get()
+        if instruction is None:
+            break
+        else:
+            __handleInstruction(instruction)
+
+
+def __handleInstruction(instruction: Instruction):
+    if instruction[0] == INSTRUCTION_PRESS:
+        print("press", instruction)
+        # press(instruction[1])
+    elif instruction[0] == INSTRUCTION_RELEASE:
+        print("release", instruction)
+        # release()
+    elif instruction[0] == INSTRUCTION_CLICK:
+        print("click", instruction)
+        # click(instruction[1], instruction[2])
+    elif instruction[0] == INSTRUCTION_MOUSE_MOVE:
+        print("mouseMove", instruction)
+        # mouseMove(instruction[1])
+
+
+def close():
+    serial.ser.close()  # type: ignore
+
+
+def press(key: str):
+    __keyboard.send_data(key)
+
+
+def release():
+    __keyboard.release()
+
+
+def click(
+    point: Screen.Point | None,
+    offset: Offset | None = None,
+):
+    if point is None:
+        return
+
+    x, y = point
+    offsetX, offsetY = 0, 0
+
+    if offset:
+        offsetX, offsetY = offset
+
+    __mouse.send_data_absolute(x + offsetX, y + offsetY)
+    __mouse.click()
+
+
+def mouseMove(point: tuple[int, int]):
+    x, y = point
+    __mouse.send_data_absolute(x, y)
