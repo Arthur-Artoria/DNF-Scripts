080ad4334707a03d0178d5fb4cf5a99fa3639fed
ArtoriaPendragon <LArtoria@hotmail.com>
2024-07-07 20:32:39 +0800

    feat: 删除容易误判的boss图片

--- src/main.py
+++ src/main.py
@@ -7,31 +7,39 @@ from services import Controller, ScreenStream
 from core.City import City
 
 
-roleIndex = 0
-while True:
-    Controller.setup()
-    roleIndex = SelectRole.selectRole(roleIndex)
-    print(roleIndex)
+def main():
+    roleIndex = 0
+    while True:
+        Controller.setup()
+        roleIndex = SelectRole.selectRole(roleIndex)
+        print(f"第 {roleIndex} 个角色")
 
-    if roleIndex == -1:
-        print("关闭")
+        if roleIndex == -1:
+            print("关闭")
+            Controller.close()
+            os.system("shutdown -s -t  10 ")
+            break
+        time.sleep(3)
+        roleOption = Roles_local.roleList[roleIndex - 1]
+        city = City()
+        dungeon = Dungeon(
+            name="Silence",
+            area="images/dungeons/1.png",
+            target="images/dungeons/mapTarget.png",
+            offset={"x": 200, "y": 10},
+            roleOption=roleOption,
+            direction="Right",
+        )
+        city.dungeon = dungeon
+        ScreenStream.listen()
+        ScreenStream.stop()
         Controller.close()
-        os.system("shutdown -s -t  10 ")
-        break
-    time.sleep(3)
-    roleOption = Roles_local.roleList[roleIndex - 1]
-    city = City()
-    dungeon = Dungeon(
-        name="Silence",
-        area="images/dungeons/1.png",
-        target="images/dungeons/mapTarget.png",
-        offset={"x": 180, "y": 10},
-        roleOption=roleOption,
-        direction="Right",
-    )
-    city.dungeon = dungeon
-    ScreenStream.listen()
-    Controller.close()
 
 
-Controller.close()
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        Controller.release()
+        Controller.close()
+        print("程序异常退出")
